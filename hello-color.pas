df424290171179604a6f7f5b4c658cec8a60a282
spartrekus <spartrekus@gmail.com>
2019-01-12 18:38:11 +0100

    Add files via upload

---
+++ hello-color.pas
@@ -0,0 +1,25 @@
+//fpc 3.0.0
+
+program HelloWorld;
+
+uses
+  Crt;
+ 
+begin
+  TextColor(LightBlue);
+  write('H');
+  TextColor(Green);
+  write('a');
+  TextColor(Cyan);
+  write('l');
+  TextColor(Red);
+  write('l');
+  TextColor(Magenta);
+  write('o');
+
+  TextColor(LightGray);
+  write(' World');
+
+  TextColor(LightGray);
+end.
+
