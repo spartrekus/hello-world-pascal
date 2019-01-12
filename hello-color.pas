c6a8f2e3570d95399a235144944c55737cde01fb
spartrekus <spartrekus@gmail.com>
2019-01-12 18:44:07 +0100

    Update hello-color.pas

--- hello-color.pas
+++ hello-color.pas
@@ -20,6 +20,16 @@ begin
   TextColor(LightGray);
   write(' World');
 
+  TextColor(White);
+  WriteLn('This is written in with the default background color');
+  TextBackground(Green);
+  WriteLn('This is written in with a Green background');
+  TextBackground(Brown);
+  WriteLn('This is written in with a Brown background');
+  TextBackground(Black);
+  WriteLn('Back with a black background');
+  ReadLn;
+
   TextColor(LightGray);
 end.
 
